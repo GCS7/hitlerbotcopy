b55a2029c9718d64ce857858b2a5f6cc63172d98
GCS7 <70507746+GCS7@users.noreply.github.com>
2020-08-31 10:27:53 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,108 @@
+configuration = {
+    # Only for testing
+    2: {
+       "roles": [
+           "Liberal",
+           "Hitler"
+       ]
+    },
+    3: {
+        "roles": [
+            "Liberal",
+            "Fascist",
+            "Hitler"
+        ]
+    },
+    4: {
+        "roles": [
+            "Liberal",
+            "Liberal",
+            "Fascist",
+            "Hitler"
+        ]
+    },
+    5: {
+        "roles": [
+            "Liberal",
+            "Liberal",
+            "Liberal",
+            "Fascist",
+            "Hitler"
+        ]
+    },
+    6: {
+        "roles": [
+            "Liberal",
+            "Liberal",
+            "Liberal",
+            "Liberal",
+            "Fascist",
+            "Hitler"
+        ]
+    },
+    7: {
+        "roles": [
+            "Liberal",
+            "Liberal",
+            "Liberal",
+            "Liberal",
+            "Fascist",
+            "Fascist",
+            "Hitler"
+        ]
+    },
+    8: {
+        "roles": [
+            "Liberal",
+            "Liberal",
+            "Liberal",
+            "Liberal",
+            "Liberal",
+            "Fascist",
+            "Fascist",
+            "Hitler"
+        ]
+    },
+    9: {
+        "roles": [
+            "Liberal",
+            "Liberal",
+            "Liberal",
+            "Liberal",
+            "Liberal",
+            "Fascist",
+            "Fascist",
+            "Fascist",
+            "Hitler"
+        ]
+    },
+    10: {
+        "roles": [
+            "Liberal",
+            "Liberal",
+            "Liberal",
+            "Liberal",
+            "Liberal",
+            "Liberal",
+            "Fascist",
+            "Fascist",
+            "Fascist",
+            "Hitler"
+        ]
+    },
+    "liberal_board": [
+        (173,90),
+        (334, 90),
+        (495, 90),
+        (657, 90),
+        (820, 90)
+    ],
+    "fascist_board": [
+        (95,90),
+        (255,90),
+        (410,90),
+        (576,90),
+        (736,90),
+        (894,90)
+    ]
+}
